bc7088e593d282b9027e6585e3ce1c63623a5351
laidouni <mohamed.laidouni@etu.u-paris.fr>
2023-10-21 15:13:48 +0200

    Documentation: project setup through Opam (2)

---
+++ .guix/manifest.scm
@@ -0,0 +1,11 @@
+(specifications->manifest
+  (list ;; hidden dependencies of opam
+        "gcc-toolchain"
+        "diffutils"
+        "patch"
+        "unzip"
+        "tar"
+        "curl"
+        "bash"
+        ;; Opam
+        "opam"))
